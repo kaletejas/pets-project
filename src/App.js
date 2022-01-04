97772ad4f4bcc85d8ba0be9c073518f3658abcdc
kaletejas <44809524+kaletejas@users.noreply.github.com>
2022-01-04 20:00:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function App() {
+  const title = "Pets";
+
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+export default App;
